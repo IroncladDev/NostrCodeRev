59a60f5cd443d7f6b86a99039ee4c76ac3ea96dd
kody.low <kodylow7@gmail.com>
2023-07-30 18:21:45 +0000

    remove build from git history

--- nostr-dvm-ts/build/src/job-types/summarization.js
+++
@@ -1,8 +0,0 @@
-import { validateJobRequest } from "../validations/index.js";
-import { inProgress } from "../jobs/reactions/in-progress.js";
-export async function onNewSummarizationJob(event) {
-    console.log("New summarization job");
-    await validateJobRequest(event);
-    inProgress(event);
-}
-//# sourceMappingURL=summarization.js.map
--- nostr-dvm-ts/build/src/validations/requester.js
+++
@@ -1,6 +0,0 @@
-export default async function validateRequester(event) {
-    if (event) {
-        /* empty */
-    }
-}
-//# sourceMappingURL=requester.js.map
