922019d87a486663ab95c1c88d93337c960d3e8a
kody.low <kodylow7@gmail.com>
2023-07-30 18:20:09 +0000

    recurse for large diffs

--- nostr-dvm-ts/src/job-types/code-review.ts
+++ nostr-dvm-ts/src/job-types/code-review.ts
@@ -12,40 +12,54 @@ export async function codeReviewJob(event: NDKEvent): Promise<string> {
     inProgress(event);
 
     return new Promise((resolve) => {
-        setTimeout(() => {
+        setTimeout(async () => {
             console.log("calling openai");
-            axios
-                .post(
-                    "https://api.openai.com/v1/chat/completions",
-                    {
-                        model: "gpt-3.5-turbo",
-                        messages: [
-                            {
-                                role: "system",
-                                content: getConfig().prompt,
-                            },
-                            {
-                                role: "user",
-                                content: event.content,
-                            },
-                        ],
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ` + process.env["OPENAI_API_KEY"],
+
+            const maxTokens = 8000;  // Maximum number of tokens for a single request
+            let content = event.content;
+            let totalTokens = Math.ceil(content.length / 3);  // Estimate tokens as length/3
+
+            let responses = [];
+            while (totalTokens > 0) {
+                console.log("Running with totalTokens: ", totalTokens);
+                let currentContent = content.slice(0, maxTokens * 3);  // Grab the current chunk of content
+                content = content.slice(maxTokens * 3);  // Update the remaining content
+                totalTokens = Math.ceil(content.length / 3);  // Update the remaining tokens
+
+                await axios
+                    .post(
+                        "https://api.openai.com/v1/chat/completions",
+                        {
+                            model: "gpt-4",
+                            messages: [
+                                {
+                                    role: "system",
+                                    content: getConfig().prompt,
+                                },
+                                {
+                                    role: "user",
+                                    content: currentContent,
+                                },
+                            ],
                         },
-                    },
-                )
-                .then((response) => {
-                    console.log(
-                        "response from openai: ",
-                        response.data.choices[0].message.content,
-                    );
-                    const content = response.data.choices[0].message.content;
-                    console.log("content", content);
-                    resolve(content);
-                });
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ` + process.env["OPENAI_API_KEY"],
+                            },
+                        },
+                    )
+                    .then((response) => {
+                        console.log(
+                            "response from openai: ",
+                            response.data.choices[0].message.content,
+                        );
+                        const responseContent = response.data.choices[0].message.content;
+                        console.log("responseContent", responseContent);
+                        responses.push(responseContent);
+                    });
+            }
+            resolve(responses.join(' '));  // Combine all responses
         }, 10000);
     });
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import "websocket-polyfill";
 import NDK, {
-  NDKEvent,
-  NDKNip07Signer,
-  NDKSubscription,
+    NDKEvent,
+    NDKNip07Signer,
+    NDKSubscription,
 } from "@nostr-dev-kit/ndk";
 import type { NostrEvent } from "@nostr-dev-kit/ndk";
 import { HandshakeStatus } from "@replit/extensions";
@@ -24,253 +24,257 @@ import { Markdown } from "./components/Markdown";
 import { useAtom } from "jotai";
 import Progress from "./components/Progress";
 import { tokens } from "./ui";
+import { Check } from "react-feather";
 
 function App() {
-  const { status } = useReplit();
+    const { status } = useReplit();
 
-  // Synchronize replit's theme with the Extension
-  const themeValues = useThemeValues();
-  const mappedThemeValues = themeValues
-    ? Object.entries(themeValues).map(
-        ([key, val]) =>
-          `--${key.replace(
-            /[A-Z]/g,
-            (c) => "-" + c.toLowerCase(),
-          )}: ${val} !important;`,
-      )
-    : [];
+    // Synchronize replit's theme with the Extension
+    const themeValues = useThemeValues();
+    const mappedThemeValues = themeValues
+        ? Object.entries(themeValues).map(
+            ([key, val]) =>
+                `--${key.replace(
+                    /[A-Z]/g,
+                    (c) => "-" + c.toLowerCase(),
+                )}: ${val} !important;`,
+        )
+        : [];
 
-  // State Atoms
-  const { ndk, setNDK } = useNDK();
-  const { status: relayStatus, setStatus, url } = useRelay();
-  const { pubKey: pk, setPubKey } = usePubKey();
-  const [progress, setProgress] = useAtom(progressAtom);
+    // State Atoms
+    const { ndk, setNDK } = useNDK();
+    const { status: relayStatus, setStatus, url } = useRelay();
+    const { pubKey: pk, setPubKey } = usePubKey();
+    const [progress, setProgress] = useAtom(progressAtom);
 
-  const [eventFeed, setEventFeed] = useState<
-    Array<{
-      status: ProgressResult;
-      event: NDKEvent;
-    }>
-  >([]);
-  const [sub, setSub] = useState<NDKSubscription | null>(null);
-  const [diffs, setDiffs] = useState<Array<DiffFile>>([]);
+    const [eventFeed, setEventFeed] = useState<
+        Array<{
+            status: ProgressResult;
+            event: NDKEvent;
+        }>
+    >([]);
+    const [paidEvents, setPaidEvents] = useState<Set<number>>(new Set());
+    const [sub, setSub] = useState<NDKSubscription | null>(null);
+    const [diffs, setDiffs] = useState<Array<DiffFile>>([]);
 
-  const handleSubmit = async () => {
-    if (!ndk) return;
+    const handleSubmit = async () => {
+        if (!ndk) return;
 
-    const { output } = await checkDiffs();
+        const { output } = await checkDiffs();
 
-    setDiffs([]);
+        setDiffs([]);
 
-    const event = new NDKEvent(ndk, {
-      kind: 68005,
-      tags: [
-        ["j", "code-review"],
-        ["bid", "10000"],
-      ],
-      content: `Here is the git diff of my code.  Please provide me with a code review:\n\n${output}`,
-    } as NostrEvent);
+        const event = new NDKEvent(ndk, {
+            kind: 68005,
+            tags: [
+                ["j", "code-review"],
+                ["bid", "10000"],
+            ],
+            content: `Here is the git diff of my code.  Please provide me with a code review:\n\n${output}`,
+        } as NostrEvent);
 
-    await event.sign();
+        await event.sign();
 
-    // check if there is a subscription running here
-    if (sub !== null) {
-      console.log("already subscribed");
-      sub.stop();
-    }
-
-    // create a subscription
-    const newSub = ndk?.subscribe(
-      {
-        ...event.filter(),
-      },
-      { closeOnEose: false, groupable: false },
-    );
-
-    setEventFeed([]);
-
-    newSub!.on("event", (event: NDKEvent) => {
-      setProgress((p) => {
-        let progressType: ProgressResult = p;
-
-        if (
-          event.tags.find(
-            (x) => x[0] === "status" && x[1] === "payment-required",
-          )
-        ) {
-          progressType = "received";
-        } else if (
-          event.tags.find((x) => x[0] === "status" && x[1] === "started")
-        ) {
-          progressType = "started";
-        } else if (
-          event.tags.find((x) => x[0] === "status" && x[1] === "success")
-        ) {
-          progressType = "success";
+        // check if there is a subscription running here
+        if (sub !== null) {
+            console.log("already subscribed");
+            sub.stop();
         }
 
-        setEventFeed((prevEventFeed) => [
-          ...prevEventFeed,
-          {
-            event,
-            status: progressType,
-          },
-        ]);
+        // create a subscription
+        const newSub = ndk?.subscribe(
+            {
+                ...event.filter(),
+            },
+            { closeOnEose: false, groupable: false },
+        );
 
-        return progressType;
-      });
-    });
-    setSub(newSub!);
+        setEventFeed([]);
 
-    console.log("signed_event", event.rawEvent());
-    await event.publish();
-  };
-  const handleZap = async (e: NDKEvent) => {
-    if (typeof window.webln === "undefined") {
-      alert(
-        "You need to use a webln enabled browser or extension to zap for these jobs! Download Alby at https://getalby.com !",
-      );
-      return;
+        newSub!.on("event", (event: NDKEvent) => {
+            setProgress((p) => {
+                let progressType: ProgressResult = p;
+
+                if (
+                    event.tags.find(
+                        (x) => x[0] === "status" && x[1] === "payment-required",
+                    )
+                ) {
+                    progressType = "received";
+                } else if (
+                    event.tags.find((x) => x[0] === "status" && x[1] === "started")
+                ) {
+                    progressType = "started";
+                } else if (
+                    event.tags.find((x) => x[0] === "status" && x[1] === "success")
+                ) {
+                    progressType = "success";
+                }
+
+                setEventFeed((prevEventFeed) => [
+                    ...prevEventFeed,
+                    {
+                        event,
+                        status: progressType,
+                    },
+                ]);
+
+                return progressType;
+            });
+        });
+        setSub(newSub!);
+
+        console.log("signed_event", event.rawEvent());
+        await event.publish();
+    };
+    const handleZap = async (e: NDKEvent, index: number) => {
+        if (typeof window.webln === "undefined") {
+            alert(
+                "You need to use a webln enabled browser or extension to zap for these jobs! Download Alby at https://getalby.com !",
+            );
+            return;
+        }
+
+        await window.webln.enable();
+        const invoice = e.tags.find((tag) => tag[0] === "amount")?.[2];
+        if (invoice) {
+            const { preimage } = await window.webln.sendPayment(invoice);
+            if (!!preimage) {
+                setPaidEvents((currentPaidEvents) => new Set([...currentPaidEvents, index]));
+            }
+        }
+    };
+
+    const showDiffs = async () => {
+        const dffs = await checkDiffs();
+
+        if (dffs.json.length) {
+            setDiffs(dffs.json);
+        }
+    };
+
+    // Initialize Public Key
+    useEffect(() => {
+        async function init() {
+            if (typeof window.nostr === "undefined") {
+                return;
+            }
+            const pubkey = await window.nostr.getPublicKey();
+            setPubKey(pubkey);
+        }
+
+        init();
+    }, [pk]);
+
+    // Initialize Relay Connection
+    useEffect(() => {
+        try {
+            const signer = new NDKNip07Signer();
+            const ndk = new NDK({
+                explicitRelayUrls: RELAYS,
+                signer,
+            });
+
+            ndk.pool.on("relay:connect", async (r: any) => {
+                setStatus("Connected");
+                console.log(`Connected to a relay ${r.url}`);
+            });
+
+            ndk.connect(2500);
+
+            setNDK(ndk);
+        } catch (e) {
+            console.log("error", e);
+        }
+    }, [url]);
+
+    if (status === HandshakeStatus.Error) {
+        return (
+            <div
+                className="flex-row"
+                css={{ alignItems: "center", justifyContent: "center", flexGrow: 1 }}
+            >
+                <div
+                    className="flex-col m8"
+                    css={{ alignItems: "center", maxWidth: 240 }}
+                >
+                    <X size={48} />
+                    <h1 css={{ fontSize: 24, fontWeight: 600 }}>Failed to Connect</h1>
+                    <p css={{ textAlign: "center" }}>
+                        This extension couldn't establish a connection with Replit.
+                    </p>
+                </div>
+            </div>
+        );
+    } else if (status === HandshakeStatus.Loading) {
+        return (
+            <div
+                className="flex-row"
+                css={{ alignItems: "center", justifyContent: "center", flexGrow: 1 }}
+            >
+                <Loader size={32} />
+            </div>
+        );
     }
 
-    await window.webln.enable();
-    const invoice = e.tags.find((tag) => tag[0] === "amount")?.[2];
-    if (invoice) {
-      await window.webln.sendPayment(invoice);
-    }
-  };
-  const showDiffs = async () => {
-    const dffs = await checkDiffs();
-
-    if (dffs.json.length) {
-      setDiffs(dffs.json);
-    }
-  };
-
-  // Initialize Public Key
-  useEffect(() => {
-    async function init() {
-      if (typeof window.nostr === "undefined") {
-        return;
-      }
-      const pubkey = await window.nostr.getPublicKey();
-      setPubKey(pubkey);
-    }
-
-    init();
-  }, [pk]);
-
-  // Initialize Relay Connection
-  useEffect(() => {
-    try {
-      const signer = new NDKNip07Signer();
-      const ndk = new NDK({
-        explicitRelayUrls: RELAYS,
-        signer,
-      });
-
-      ndk.pool.on("relay:connect", async (r: any) => {
-        setStatus("Connected");
-        console.log(`Connected to a relay ${r.url}`);
-      });
-
-      ndk.connect(2500);
-
-      setNDK(ndk);
-    } catch (e) {
-      console.log("error", e);
-    }
-  }, [url]);
-
-  if (status === HandshakeStatus.Error) {
     return (
-      <div
-        className="flex-row"
-        css={{ alignItems: "center", justifyContent: "center", flexGrow: 1 }}
-      >
-        <div
-          className="flex-col m8"
-          css={{ alignItems: "center", maxWidth: 240 }}
-        >
-          <X size={48} />
-          <h1 css={{ fontSize: 24, fontWeight: 600 }}>Failed to Connect</h1>
-          <p css={{ textAlign: "center" }}>
-            This extension couldn't establish a connection with Replit.
-          </p>
-        </div>
-      </div>
-    );
-  } else if (status === HandshakeStatus.Loading) {
-    return (
-      <div
-        className="flex-row"
-        css={{ alignItems: "center", justifyContent: "center", flexGrow: 1 }}
-      >
-        <Loader size={32} />
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <style>{`:root, .replit-ui-theme-root {
+        <>
+            <style>{`:root, .replit-ui-theme-root {
 ${mappedThemeValues.join("\n")}
         }`}</style>
-      <main className="flex-col">
-        <Header />
+            <main className="flex-col">
+                <Header />
 
-        <div className="flex-col m8 p8">
-          {relayStatus !== "Connected" ? <RelayUnReadyState /> : null}
+                <div className="flex-col m8 p8">
+                    {relayStatus !== "Connected" ? <RelayUnReadyState /> : null}
 
-          <div className="flex-col m8">
-            <div className="flex-row m8">
-              <Select options={["Code Review"]} css={{ flexGrow: 1 }} />
-              <Button text="I'm Ready" onClick={showDiffs} />
-            </div>
-          </div>
-        </div>
+                    <div className="flex-col m8">
+                        <div className="flex-row m8">
+                            <Select options={["Code Review"]} css={{ flexGrow: 1 }} />
+                            <Button text="I'm Ready" onClick={showDiffs} />
+                        </div>
+                    </div>
+                </div>
 
-        {diffs.length > 0 ? (
-          <Diffs diffs={diffs} onSubmit={handleSubmit} onRetry={showDiffs} />
-        ) : null}
+                {diffs.length > 0 ? (
+                    <Diffs diffs={diffs} onSubmit={handleSubmit} onRetry={showDiffs} />
+                ) : null}
 
-        {progress ? <Progress progress={progress} /> : null}
+                {progress ? <Progress progress={progress} /> : null}
 
-        {eventFeed.length > 0 && (
-          <div className="p8">
-            {eventFeed
-              .filter(({ status }) => status === progress)
-              .map(({ event }, index) => {
-                // Extract the necessary tag data
-                const amountSats =
-                  Number(
-                    event.tags.find((tag) => tag[0] === "amount")?.[1] || 0,
-                  ) / 1000;
+                {eventFeed
+                    .filter(({ status }) => status === progress)
+                    .map(({ event }, index) => {
+                        // Extract the necessary tag data
+                        const amountSats =
+                            Number(
+                                event.tags.find((tag) => tag[0] === "amount")?.[1] || 0,
+                            ) / 1000;
 
-                return (
-                  <div
-                    className="flex-row m8 p8"
-                    key={index}
-                    css={{
-                      background: tokens.backgroundHigher,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <Markdown markdown={event.content} />
-                    {amountSats > 0 ? (
-                      <Button
-                        onClick={() => handleZap(event)}
-                        text={`Zap ⚡ ${amountSats}`}
-                      />
-                    ) : null}
-                  </div>
-                );
-              })}
-          </div>
-        )}
-      </main>
-    </>
-  );
+                        return (
+                            <div
+                                className="flex-row m8 p8"
+                                key={index}
+                                css={{
+                                    background: tokens.backgroundHigher,
+                                    borderRadius: 8,
+                                }}
+                            >
+                                <Markdown markdown={event.content} />
+                                {amountSats > 0 ? (
+                                    paidEvents.has(index) ? <Check /> :
+                                        <Button
+                                            onClick={() => handleZap(event, index)}
+                                            text={`Zap ⚡ ${amountSats}`}
+                                        />
+                                ) : null}
+                            </div>
+                        );
+                    })}
+
+            </main>
+        </>
+    );
 }
 
 export default App;
